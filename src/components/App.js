766a68d1aa45d199acef20d0d4b8b408415b5f86
Rahul <royrahulrk12@gmail.com>
2023-02-09 12:43:33 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const handleClick = () => {
+  document.getElementById("para").style.display = "block";
+  }
+
   return (
     <div id="main">
       // Do not alter the main div
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={handleClick}></button>
     </div>
   );
 }
